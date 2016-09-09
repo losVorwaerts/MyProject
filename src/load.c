fc08336b657841ed2d9db9924366002fb0f6f935
losVorwaerts <zxp_jk@163.com>
2016-09-09 19:02:14 +0800

    更新load. and upload.c

--- src/load.c
+++ src/load.c
@@ -9,21 +9,23 @@ static const char rcsid[] = "$Id: echo.c,v 1.5 1999/07/28 00:29:37 roberts Exp $
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-
-#ifdef _WIN32
-#include <process.h>
-#else
-extern char **environ;
-#endif
+#include <sys/wait.h>
 
 #include "fcgi_stdio.h"
 #include "make_log.h"
+#include "upload.h"
+#include "redis_op.h"
 
 
+#define LOAD_LOG_MODULE "cgi"
+#define LOAD_LOG_PROC   "load"
+
+#define	TEMP_BUF_MAX_LEN		256
+#define	HOST_NAME_LEN		30
+
+#define REDIS_SERVER_IP     "127.0.0.1"
+#define REDIS_SERVER_PORT   "6379"
 
 //find 'substr' from a fixed-length buffer   
 //('full_data' will be treated as binary data buffer)  
@@ -56,6 +58,227 @@ char* memstr(char* full_data, int full_data_len, char* substr)
 
     return NULL; 
 } 
+/**
+ * @brief  得到文件后缀字符串 如果非法文件后缀,返回"null"
+ */
+int get_file_suffix(const char *file_name, char *suffix)
+{
+    const char *p = file_name;
+    int len = 0;
+    const char *q=NULL;
+    const char *k= NULL;
+
+    if (p == NULL) {
+        return -1;
+    }
+
+    q = p;
+
+    //asdsd.doc.png
+    //             ↑
+
+    while (*q != '\0') {
+        q++;
+    }
+
+    k = q;
+    while (*k != '.' && k != p) {
+        k--;
+    }
+
+    if (*k == '.') {
+        k++;
+        len = q - k;
+
+        if (len != 0) {
+            strncpy(suffix, k, len);
+            suffix[len] = '\0';
+        }
+        else {
+            strncpy(suffix, "null", 5);
+        }
+    }
+    else {
+        strncpy(suffix, "null", 5);
+    }
+
+    return 0;
+}     
+
+
+/**
+ * @brief  解析url query 类似 abc=123&bbb=456 字符串
+ *          传入一个key,得到相应的value
+ * @returns   
+ *          0 成功, -1 失败
+ */
+int query_parse_key_value(const char *query, const char *key, char *value, int *value_len_p)
+{
+    char *temp = NULL;
+    char *end = NULL;
+    int value_len =0;
+
+
+    //找到是否有key
+    temp = strstr(query, key);
+    if (temp == NULL) {
+        //LOG(UTIL_LOG_MODULE, UTIL_LOG_PROC, "Can not find key %s in query\n", key);
+
+        return -1;
+    }
+
+    temp+=strlen(key);//=
+    temp++;//value
+
+
+    //get value
+    end = temp;
+
+    while ('\0' != *end && '#' != *end && '&' != *end ) {
+        end++;
+    }
+
+    value_len = end-temp;
+
+    strncpy(value, temp, value_len);
+    value[value_len] ='\0';
+
+    if (value_len_p != NULL) {
+        *value_len_p = value_len;
+    }
+
+    return 0;
+}
+
+
+int make_file_url(char *fileid, char *fdfs_file_url)
+{
+	int		ret = 0;
+	
+	char	*p = NULL;
+	char	*q = NULL;
+	char	*k = NULL;
+	
+	char fdfs_file_stat_buf[TEMP_BUF_MAX_LEN] = {0};
+    char fdfs_file_host_name[HOST_NAME_LEN] = {0};
+
+    int pfd[2];
+    pid_t pid;
+
+    pipe(pfd);
+    pid = fork();
+
+    if(pid == 0)
+    {
+        close(pfd[0]);
+        dup2(pfd[1],STDOUT_FILENO);
+        LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,"[%s,%d]",fileid,strlen(fileid));
+
+        execl("/usr/bin/fdfs_file_info","fdfs_file_info","/etc/fdfs/client.conf",fileid,NULL);
+        LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,"execl err");
+    }
+
+    close(pfd[1]);
+
+    read(pfd[0],fdfs_file_stat_buf,TEMP_BUF_MAX_LEN);
+    LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,"get file_ip [%s] succ", fdfs_file_stat_buf);
+
+    wait(NULL);
+
+    p = strstr(fdfs_file_stat_buf, "source ip address: ");
+
+    q = p + strlen("source ip address: ");
+    k = strstr(q,"\n");
+    strncpy(fdfs_file_host_name, q ,k-q);
+
+    fdfs_file_host_name[k-q] = '\0';
+
+    strcat(fdfs_file_url,"http://");
+    strcat(fdfs_file_url, fdfs_file_host_name);
+    strcat(fdfs_file_url,"/");
+    strcat(fdfs_file_url, fileid);
+
+    LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,"[%s]",fdfs_file_url);
+
+    return ret;
+}
+int get_username(char *user)
+{
+    char *query_string = getenv("QUERY_STRING");
+
+    query_parse_key_value(query_string, "user", user, NULL);
+    if(strlen(user) == 0)
+    {
+        LOG(LOAD_LOG_MODULE, LOAD_LOG_PROC,"get user has no value!");
+        return -1;
+    }
+    else{
+        LOG(LOAD_LOG_MODULE, LOAD_LOG_PROC,"get user = [%s]",user);
+    }
+
+    return 0;
+}
+
+int store_file_to_redis(char *fileid, char *fdfs_file_url, char *filename, char *user)
+{
+    int     ret = 0;
+    redisContext *redis_conn = NULL;
+
+    char *redis_value_buf = NULL;
+    time_t now;
+    char create_time[25] = {0};
+    char suffix[8];
+
+    redis_conn = rop_connectdb_nopwd(REDIS_SERVER_IP, REDIS_SERVER_PORT);
+    if(redis_conn == NULL)
+        return ret;
+
+    redis_value_buf = malloc(VALUES_ID_SIZE);
+    if(redis_value_buf == NULL)
+    {
+        LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,"malloc redis value error");
+        ret = -1;
+        goto END;
+    }
+
+    memset(redis_value_buf, 0, VALUES_ID_SIZE);
+
+    strcat(redis_value_buf, fileid);
+    strcat(redis_value_buf,"|");
+
+    strcat(redis_value_buf, fdfs_file_url);
+    strcat(redis_value_buf,"|");
+
+    strcat(redis_value_buf, filename);
+    strcat(redis_value_buf,"|");
+
+    now = time(NULL);
+    strftime(create_time, 24,"%Y-%m-%d %H:%M:%S", localtime(&now));
+    strcat(redis_value_buf, create_time);
+    strcat(redis_value_buf,"|");
+    strcat(redis_value_buf,user);
+    strcat(redis_value_buf,"|");
+
+    get_file_suffix(filename, suffix);
+
+    strcat(redis_value_buf, suffix);
+
+    rop_list_push(redis_conn,"FILE_INFO_LIST",redis_value_buf);
+
+    rop_zset_increment(redis_conn, "FILE_HOT_ZSET",fileid);
+
+    free(redis_value_buf);
+
+END:
+    rop_disconnect(redis_conn);
+    return ret;
+}
+
+int store_file(char *fileid, char *fdfs_file_url, char *filename, char *user)
+{
+    return store_file_to_redis(fileid, fdfs_file_url, filename, user);
+}
+
 
 int main ()
 {	
@@ -66,6 +289,10 @@ int main ()
     char *boundary = NULL;
     char *filenamestr = NULL;
 	char filename[1024]={0};
+
+    char fileid[TEMP_BUF_MAX_LEN] = {0};
+    char fdfs_file_url[512] = {0};
+    char user[128] = {0};
 	
     while (FCGI_Accept() >= 0) {
         char *contentLength = getenv("CONTENT_LENGTH");
@@ -111,24 +338,41 @@ int main ()
             filenamestr++;
             i++;
         }
-		LOG("LOAD","MAIN",filename);
+		LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,filename);
         char *start=memstr(buf,len,"\r\n\r\n");
         start+=4;
         
         int newlen =len-(int)(start-buf);
         char *end = memstr(start,newlen,boundary);
         end-=2;
-
+		
+		//char	path[
         int fd = open(filename,O_RDWR|O_CREAT|O_TRUNC,0664);
         len =(int) (end-start);
 
         write(fd,start,len);
+        
         if(buf!=NULL)
         {
             free(buf);
         }
         close(fd);
-        
+        upload(filename,fileid);
+        LOG(LOAD_LOG_MODULE,LOAD_LOG_PROC,fileid);
+
+        make_file_url(fileid, fdfs_file_url);
+
+        unlink(filename);
+        if(store_file(fileid, fdfs_file_url, filename, user) < 0)
+        {
+            goto END;
+        }
+END:
+        memset(filename,0,sizeof(filename));
+        memset(fileid, 0, sizeof(fileid));
+        memset(fdfs_file_url, 0, sizeof(fdfs_file_url));
+        memset(user, 0,sizeof(user));
+
     } 
 
     return 0;
--- src/upload.c
+++ src/upload.c
@@ -3,23 +3,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <string.h>
 
 #include "make_log.h"
 
-#define MAXLINE 1024
 
-int main(int argc,char *argv[])
+int upload(char *filename,char *fileid)
 {
     //int n=0;
     pid_t pid;
     int fd[2];
-    char line[MAXLINE];
     
-    if(argc<2)
-    {
-        printf("usage ./upload filename\n");
-        exit(1);
-    }
+  
     if(pipe(fd)<0)
     {
        // perror("pipe");
@@ -38,14 +33,17 @@ int main(int argc,char *argv[])
         close(fd[0]);
         dup2(fd[1],STDOUT_FILENO);
         
-        execl("/usr/bin/fdfs_upload_file","fdfs_upload_file","/etc/fdfs/client.conf",argv[1],NULL);
+        execl("/usr/bin/fdfs_upload_file","fdfs_upload_file","/etc/fdfs/client.conf",filename,NULL);
         LOG("fast","upload","exec err");
     }
     else{
         close(fd[1]);
-        read(fd[0],line,MAXLINE);
-        LOG("fast","upload","%s",line);
+        read(fd[0],fileid,256);
+        fileid[strlen(fileid)-1] = '\0';
+        LOG("fast","upload","%s",fileid);
+
         wait(NULL);
+        close(fd[0]);
     }
 
     return 0;
