ac54afeb2353c7a790098dbdfb62859f253713c5
losVorwaerts <zxp_jk@163.com>
2016-09-09 19:08:56 +0800

    更新start-up.sh

--- start-up.sh
+++ start-up.sh
@@ -2,4 +2,5 @@
 #spawn-fcgi -a 127.0.0.1 -p 8081 -f ./demo
 #spawn-fcgi -a 127.0.0.1 -p 8082 -f ./echo_test
 #spawn-fcgi -a 127.0.0.1 -p 8082 -f ./load_get
-spawn-fcgi -a 127.0.0.1 -p 8082 -f ./load
+#spawn-fcgi -a 127.0.0.1 -p 8082 -f ./load
+spawn-fcgi -a 127.0.0.1 -p 8083 -f ./data_cgi
